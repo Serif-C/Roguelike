f6f47b5dca55cf2f9a75bae3f7cce0007961418a
Serif-C <92169062+Serif-C@users.noreply.github.com>
2025-07-15 02:36:24 -0400

    Unity Project +  Movement and Animation

---
+++ BOI_OnePiece/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] Animator anim;
+
+    private Vector2 movementInput;
+    private InputSystem_Actions inputSystem;
+
+    private void Awake()
+    {
+        inputSystem = new InputSystem_Actions(); // instantiate the input system
+
+        // Subscribe to movement action
+        inputSystem.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        inputSystem.Player.Move.canceled += ctx => movementInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        inputSystem.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputSystem.Disable();
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("xInput", movementInput.x);
+        anim.SetFloat("yInput", movementInput.y);
+
+        Vector2 movement = movementInput * movementSpeed * Time.deltaTime ;
+
+        transform.position += (Vector3) movement;
+    }
+}
