ab65e3d2fbb24bbd9b5fd404951a20da0372e15a
Serif-C <92169062+Serif-C@users.noreply.github.com>
2025-07-18 18:11:10 -0400

    Laid out requirements for basic player combat script

---
+++ BOI_OnePiece/Assets/Scripts/Combat.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+/*
+ * !!!Think about controller support!!!
+ * 
+ * Basic Attack:
+ * - Gumpistol: Shoot out your hands alternately (base on aspd) to the direction you are looking
+ *
+ * Heavy Attack:
+ * - Gumbazooka: Shoot out both your hands in the direction you are looking (2s cooldown)
+ * 
+ * Attacks should be physics based and should ricochet upon collision.
+ * Since attacks stretches, it should feel snappy, and have a lot of weight on impact.
+ */
+}
